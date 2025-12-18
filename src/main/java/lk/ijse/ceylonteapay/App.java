50db780260523a58968e88430ff76d70192d9f32
nadeeja <nadeejaamaraweera@gmail.com>
2025-12-18 12:39:00 +0530

    new changes

--- src/main/java/lk/ijse/ceylonteapay/App.java
+++ src/main/java/lk/ijse/ceylonteapay/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 //        scene = new Scene(loadFXML("Login"), 600, 334);
-        scene = new Scene(loadFXML("Login"), 1500, 800);
+        scene = new Scene(loadFXML("MainLogin"), 891, 633);
 //        scene.getStylesheets().add(getClass().getResource("Style.css").toExternalForm());
         stage.setScene(scene);
         stage.setResizable(false);
